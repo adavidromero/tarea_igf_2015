035d7a8f646873cb2121495b6b0e724a3403f5d5
David Romero <rp07004@ues.edu.sv>
2015-11-02 14:26:47 -0600

    Arreglado Dao Generico, agregado AplicativoDAO, cambios en web.xml y agregada libreria commons-logging-1.1.2.jar

--- ProyectoTareaIGF2015/src/com/fia/igf/app/datos/AplicativoDAO.java
+++ ProyectoTareaIGF2015/src/com/fia/igf/app/datos/AplicativoDAO.java
@@ -2,8 +2,12 @@ package com.fia.igf.app.datos;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,33 +15,62 @@ import com.fia.igf.app.dominio.Aplicativo;
 import com.fia.igf.utilidades.datos.HibernateUtil;
 
 @Repository
-public class AplicativoDAO implements GenericDAO<Aplicativo, Short>{
+public class AplicativoDAO implements GenericDAO<Aplicativo, String>{
 	@Autowired
 	private HibernateUtil hibernateUtil;
 
-	@Override
-	public void guardar(Aplicativo obj) {
-		// TODO Auto-generated method stub
-		
+	private SessionFactory sessionFactory = hibernateUtil.getSessionFactory();
+	private Session sesion;
+	private Transaction tx;
+
+	private void iniciaOperacion() throws HibernateException{
+		sesion = sessionFactory.openSession();
+		tx = sesion.beginTransaction();
 	}
 
 	@Override
-	public void eliminar(Aplicativo obj) {
-		// TODO Auto-generated method stub
-		
+	public void guardarActualiza(Aplicativo aplicativo) {
+		try{
+			iniciaOperacion();
+			sesion.saveOrUpdate(aplicativo);
+			tx.commit();
+			sesion.flush();
+		}catch(HibernateException he){
+			manejaExcepcion(he);
+			throw he;
+		}finally{
+			sesion.close();
+		}
+	}
+
+	@Override
+	public void eliminar(Aplicativo aplicativo) {
+		iniciaOperacion();
+	    sesion.delete(aplicativo) ;
+	    tx.commit() ;
+	    sesion.flush() ;
+	    sesion.close() ;
 	}
 
 	@Override
 	public List<Aplicativo> obtenerTodos(Class<Aplicativo> clazz) {
-		// TODO Auto-generated method stub
-		return null;
+		iniciaOperacion();
+		Criteria criteria = sesion.createCriteria(clazz);
+		sesion.close();
+		return (List<Aplicativo>)criteria.list();
+	}
+
+	private void manejaExcepcion(HibernateException he) throws HibernateException{
+		tx.rollback();
+		throw new HibernateException("Ocurrió un error en la capa DAO",he);
 	}
 
 	@Override
-	public Aplicativo obtenerPorId(Aplicativo obj) {
-		// TODO Auto-generated method stub
-		return null;
+	public Aplicativo obtenerPorId(Class<Aplicativo> clazz, String id) {
+		iniciaOperacion();
+		Aplicativo aplicativo = (Aplicativo)sesion.get(clazz, id);
+		sesion.close();
+		return aplicativo;
 	}
-	
 
 }
--- ProyectoTareaIGF2015/src/com/fia/igf/app/datos/GenericDAO.java
+++ ProyectoTareaIGF2015/src/com/fia/igf/app/datos/GenericDAO.java
@@ -2,10 +2,12 @@ package com.fia.igf.app.datos;
 
 import java.util.List;
 
+import com.fia.igf.app.dominio.Aplicativo;
+
 public interface GenericDAO<T,PK> {
-	public 	void guardar (T obj);
+	public 	void guardarActualiza (T obj);
 	public 	void eliminar (T obj);
 	public List<T> obtenerTodos(Class<T> clazz);
-	public T obtenerPorId(T obj);
+	public T obtenerPorId(Class<T> clazz, PK id);
 
 }
