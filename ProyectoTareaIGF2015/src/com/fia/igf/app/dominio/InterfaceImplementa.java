a51250f14972986495b4f185be4c2b261358725a
Carlos Arias <carlosa.ariaslopez@gmail.com>
2015-11-13 11:14:45 -0600

    Cambios menores
    Cambios para solventar un problema pequeño

--- ProyectoTareaIGF2015/src/com/fia/igf/app/dominio/InterfaceImplementa.java
+++ ProyectoTareaIGF2015/src/com/fia/igf/app/dominio/InterfaceImplementa.java
@@ -13,27 +13,34 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="as_interface_implementa")
-public class InterfaceImplementa implements Serializable{
+@Table(name = "as_interface_implementa")
+public class InterfaceImplementa implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Basic(optional = false)
-    @Column(name = "c_interface_implementa")
+	@Column(name = "c_interface_implementa")
 	private String cInterfaceImplementa = new String();
-	
+
 	@ManyToOne
 	@JoinColumn(name = "c_interface_hijo", referencedColumnName = "c_interface")
 	private Interface interfaceHijo;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "c_interface_padre", referencedColumnName = "c_interface")
 	private Interface interfacePadre;
-	
-	public InterfaceImplementa(){
-		
+
+	public InterfaceImplementa() {
+
+	}
+
+	public InterfaceImplementa(String cInterfaceImplementa,
+			Interface interfaceHijo, Interface interfacePadre) {
+		this.cInterfaceImplementa = cInterfaceImplementa;
+		this.interfaceHijo = interfaceHijo;
+		this.interfacePadre = interfacePadre;
 	}
 
 	public String getcInterfaceImplementa() {
@@ -47,6 +54,7 @@ public class InterfaceImplementa implements Serializable{
 	public Interface getInterfaceHijo() {
 		return interfaceHijo;
 	}
+
 	public void setInterfaceHijo(Interface interfaceHijo) {
 		this.interfaceHijo = interfaceHijo;
 	}
@@ -54,10 +62,9 @@ public class InterfaceImplementa implements Serializable{
 	public Interface getInterfacePadre() {
 		return interfacePadre;
 	}
+
 	public void setInterfacePadre(Interface interfacePadre) {
 		this.interfacePadre = interfacePadre;
 	}
-	
-	
 
 }
